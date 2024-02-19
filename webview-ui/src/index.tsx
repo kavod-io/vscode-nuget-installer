59c2575167284a0712669b7c68ac49fdd1295168
Ross Knudsen <rossknudsen@gmail.com>
2024-02-19 20:47:28 +1300

    fix: update react app mounting

--- webview-ui/src/index.tsx
+++ webview-ui/src/index.tsx
@@ -1,15 +1,16 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import { App } from "./components/App"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import React from "react"
+import { createRoot } from "react-dom/client"
+import { App } from "./components/App"
 
 const queryClient = new QueryClient()
 
-ReactDOM.render(
+const container = document.getElementById("root")
+const root = createRoot(container!)
+root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 )
