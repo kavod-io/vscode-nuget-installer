f549acbef3b89f64b9dfa46bef37e069161a7f78
Ross Knudsen <rossknudsen@gmail.com>
2024-03-23 15:21:53 +1300

    rename project since its already used.

--- src/extension.ts
+++ src/extension.ts
@@ -2,12 +2,9 @@ import { commands, ExtensionContext } from "vscode"
 import { NugetPackagePanel } from "./panels/NugetPackagePanel"
 
 export function activate(context: ExtensionContext) {
-  const showNugetGuiCommand = commands.registerCommand(
-    "vscode-nuget-package-manager.showGui",
-    () => {
-      NugetPackagePanel.render(context.extensionUri)
-    }
-  )
+  const showNugetGuiCommand = commands.registerCommand("vscode-nuget-installer.showGui", () => {
+    NugetPackagePanel.render(context.extensionUri)
+  })
 
   context.subscriptions.push(showNugetGuiCommand)
 }
